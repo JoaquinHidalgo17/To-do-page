9f55ec3500c691f1ffd5a01101f7e345b571a19a
Coqui <joaquinhidalgo17gmail.com>
2022-11-28 21:35:40 -0300

    create function to update the list

--- back.js
+++ back.js
@@ -11,8 +11,25 @@ for (let i = 0; i <= 2; i++) {
     tarea.innerText = actividades[i].actividad;
     listatareas.appendChild(tarea);
 } */
+let maxId = 0
+function createClaim(title){
+    maxId++ ;
+ let newClaim = {id: maxId ,title: title , note: "" } ;
+ lista.push(newClaim);
+};
+function deleteClaim(){};
 
-let lista = {};
+function updateClaim(id, title, note){
+
+}
+let lista = [
+    {
+        id: 0,
+        title: "Tarea 1", 
+        note: "notas de la tarea",
+        
+    }
+];
 
 
 let textValue = document.getElementById("newtext");
@@ -20,6 +37,8 @@ let divInput = document.getElementById("divinput");
 
 
 function onTaskCreate() {
+    createClaim(textValue.value);
+    console.log(lista)
     if (textValue.value == false) {
         alert("Tarea Vacia");
     } else {
